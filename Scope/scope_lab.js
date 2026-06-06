7d3dcc75391ce4d629e44b9ee53043428056de7d
Amaan-800 <amaanmohammad1111@gmail.com>
2026-06-06 18:15:38 +0530

    Completed scope lab

--- Scope/scope_lab.js
+++ Scope/scope_lab.js
@@ -13,3 +13,17 @@ const blockConst = "I'm a block-scoped const";
 console.log(globalVar);
 console.log(globalLet);
 console.log(globalConst);
+// Block Scope
+//console.log(blockVar);
+//console.log(blockLet);
+function show(){
+    var functionVar = "I'm a block-scoped var";
+    let functionLet = "I'm a block-scoped let";
+    const functionConst = "I'm a block-scoped const";
+}
+
+show();
+
+console.log(functionVar);
+console.log(functionLet);
+console.log(functionConst);
